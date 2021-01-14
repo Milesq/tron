05348f69041d45374aed59f5bb406f5d09a7f53b
Milesq <wmilosz88@gmail.com>
2021-01-14 23:29:17 +0100

    feat: temporary disabled displayer

--- displayer/console.go
+++ displayer/console.go
@@ -1,12 +1,10 @@
 package displayer
 
 import (
-	"fmt"
-
 	"github.com/milesq/tron/tron"
 )
 
 // ConsoleDisplayer .
 func ConsoleDisplayer(state tron.GameState) {
-	fmt.Println(state)
+	// fmt.Println(state)
 }

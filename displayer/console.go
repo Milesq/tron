1ec768cff786c24312781e3b02280742faa4042c
Milesq <wmilosz88@gmail.com>
2021-01-14 19:17:02 +0100

    feat: add basic displayer

--- displayer/console.go
+++ displayer/console.go
@@ -1,3 +1,12 @@
 package displayer
 
-func ConsoleDisplayer() {}
+import (
+	"fmt"
+
+	"github.com/milesq/tron/tron"
+)
+
+// ConsoleDisplayer .
+func ConsoleDisplayer(state tron.GameState) {
+	fmt.Println(state)
+}

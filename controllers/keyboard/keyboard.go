1fdb65088572790f0a0f98d01545806e8a759321
Milesq <wmilosz88@gmail.com>
2021-01-15 17:03:49 +0100

    fix: keyboard emitter

--- controllers/keyboard/keyboard.go
+++ controllers/keyboard/keyboard.go
@@ -16,7 +16,7 @@ func Controller(game *tron.Game) {
 	defer keyboard.Close()
 
 	for {
-		_, key, err := keyboard.GetKey()
+		char, key, err := keyboard.GetKey()
 
 		if err != nil {
 			fmt.Println("Keyboard listener disabled")
@@ -28,48 +28,27 @@ func Controller(game *tron.Game) {
 			game.Emit(event.Exit, -1)
 			break
 
-			// ██╗   ██╗██████╗
-			// ██║   ██║██╔══██╗
-			// ██║   ██║██████╔╝
-			// ██║   ██║██╔═══╝
-			// ╚██████╔╝██║
-			//  ╚═════╝ ╚═╝
 		case keyboard.KeyArrowUp:
 			game.Emit(event.Up, 0)
-		case keyboard.KeyCtrlW:
-			game.Emit(event.Up, 1)
 
-			// ██████╗ ██╗ ██████╗ ██╗  ██╗████████╗
-			// ██╔══██╗██║██╔════╝ ██║  ██║╚══██╔══╝
-			// ██████╔╝██║██║  ███╗███████║   ██║
-			// ██╔══██╗██║██║   ██║██╔══██║   ██║
-			// ██║  ██║██║╚██████╔╝██║  ██║   ██║
-			// ╚═╝  ╚═╝╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝
 		case keyboard.KeyArrowRight:
 			game.Emit(event.Right, 0)
-		case keyboard.KeyCtrlD:
-			game.Emit(event.Right, 1)
 
-			// ██████╗  ██████╗ ██╗    ██╗███╗   ██╗
-			// ██╔══██╗██╔═══██╗██║    ██║████╗  ██║
-			// ██║  ██║██║   ██║██║ █╗ ██║██╔██╗ ██║
-			// ██║  ██║██║   ██║██║███╗██║██║╚██╗██║
-			// ██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║
-			// ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝
 		case keyboard.KeyArrowDown:
 			game.Emit(event.Down, 0)
-		case keyboard.KeyCtrlS:
-			game.Emit(event.Down, 1)
 
-			// ██╗     ███████╗███████╗████████╗
-			// ██║     ██╔════╝██╔════╝╚══██╔══╝
-			// ██║     █████╗  █████╗     ██║
-			// ██║     ██╔══╝  ██╔══╝     ██║
-			// ███████╗███████╗██║        ██║
-			// ╚══════╝╚══════╝╚═╝        ╚═╝
 		case keyboard.KeyArrowLeft:
 			game.Emit(event.Left, 0)
-		case keyboard.KeyCtrlA:
+		}
+
+		switch char {
+		case 'W', 'w':
+			game.Emit(event.Up, 1)
+		case 'S', 's':
+			game.Emit(event.Down, 1)
+		case 'D', 'd':
+			game.Emit(event.Down, 1)
+		case 'A', 'a':
 			game.Emit(event.Left, 1)
 		}
 	}

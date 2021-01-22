0563276429e2c86e32a41d8ee323cc2e08087c79
Milesq <wmilosz88@gmail.com>
2021-01-22 12:29:42 +0100

    feat: add negative points when player lost with himself tail

--- tron/lost.go
+++ tron/lost.go
@@ -1,10 +1,12 @@
 package tron
 
-import "fmt"
-
 // PlayerLost .
 func (tr *Game) PlayerLost(playerID, conflictedWith int) {
 	delete(tr.State.Players, playerID)
-	fmt.Println(tr.State.Players)
-	tr.State.Points[conflictedWith]++
+
+	if playerID != conflictedWith {
+		tr.State.Points[conflictedWith] += 10
+	} else {
+		tr.State.Points[playerID] -= 5
+	}
 }
